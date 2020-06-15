79743b90425da9e18ab9189f95696445cb146a5b
Maxime <maxringue@gmail.Com>
2020-06-15 14:58:48 +0200

    POM.XML, application.yml, ClientControl.java

--- src/main/java/com/miage/td/bank/app/rest/ClientControl.java
+++ src/main/java/com/miage/td/bank/app/rest/ClientControl.java
@@ -4,24 +4,21 @@ import com.miage.td.bank.app.entities.Client;
 import com.miage.td.bank.app.repository.ClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/clients")
 public class ClientControl {
 
     @Autowired
     private ClientRepo clientRepo;
 
-    @PostMapping("/")
-    Client postClient(Client client){
+    @PostMapping("")
+    Client postClient(@RequestBody Client client){
         return this.clientRepo.save(client);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     Client getClient(@PathVariable("id") Client c){
         return c;
     }
