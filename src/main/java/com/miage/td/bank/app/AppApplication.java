3a1c41b78daaa65c1524c3285d56b43dabadf6e4
Maxime <maxringue@gmail.Com>
2020-06-25 16:40:12 +0200

    Annuaire

--- src/main/java/com/miage/td/bank/app/AppApplication.java
+++ src/main/java/com/miage/td/bank/app/AppApplication.java
@@ -3,9 +3,12 @@ package com.miage.td.bank.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+
+@EnableDiscoveryClient
 public class AppApplication {
 
 	public static void main(String[] args) {
